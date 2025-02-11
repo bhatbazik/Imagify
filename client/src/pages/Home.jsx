a741a5b701b31a17cfa1717f8ec817bc24fa1d7c
bhatbazik <bhatbazik@gmail.com>
2025-02-11 12:33:20 +0530

    added header and steps pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Header />
+      <Steps />
+      
     </div>
   )
 }
