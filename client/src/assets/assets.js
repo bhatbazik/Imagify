c431f7ec558dce3719ee9dd3bc83cf50e1a96045
bhatbazik <bhatbazik@gmail.com>
2025-02-14 18:56:28 +0530

    frontend done

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -18,6 +18,8 @@ import cross_icon from './cross_icon.svg'
 import star_group from './star_group.png'
 import credit_star from './credit_star.svg'
 import profile_icon from './profile_icon.png'
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
 
 export const assets = {
     logo,
@@ -34,7 +36,9 @@ export const assets = {
     cross_icon,
     star_group,
     credit_star,
-    profile_icon
+    profile_icon,
+    razorpay_logo,
+    stripe_logo
 }
 
 export const stepsData = [
